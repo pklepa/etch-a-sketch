3ab23f38e4c2a9c5a28993359152aeea542956d3
klepa <pedro.klepa@gmail.com>
2020-06-12 18:59:50 -0300

    Add modals and text for About and Help buttons on header

--- scripts/main.js
+++ scripts/main.js
@@ -26,6 +26,8 @@ const btnBlack = document.getElementById('btn-black');
 const btnEraser = document.getElementById('btn-eraser');
 const btnRainbow = document.getElementById('btn-rainbow');
 
+gridSizeInput.addEventListener('change', adjustGridSize);
+
 btnBlack.addEventListener('click', () => {
     currentColoring.type = 'simple';
     currentColoring.color = coloringTypes.black;
@@ -63,9 +65,22 @@ document.getElementById('btn-reset').addEventListener('click', () => {
     }
 });
 
-gridSizeInput.addEventListener('change', adjustGridSize);
 
+// Related to the modals
+const modal = document.querySelector('#modal');
+let modalTitle = document.querySelector('#modal .header h1');
+let modalBody = document.querySelector('#modal .body');
 
+const about = document.querySelector('#about');
+about.addEventListener('click', handleAboutClick);
+
+const help = document.querySelector('#help');
+help.addEventListener('click', handleHelpClick);
+
+const modalCloseBtn = document.querySelector('#modal a');
+modalCloseBtn.addEventListener('click', () => {
+    modal.classList.add('hide');
+})
 
 
 
@@ -77,8 +92,7 @@ function gridItemHover(event) {
 
     item.style = `
         background-color: ${currentColoring.type == 'simple' ? currentColoring.color : currentColoring.color()};
-    `;
-    
+    `;    
 }
 
 function createDefaultGrid() {
@@ -95,34 +109,6 @@ function createDefaultGrid() {
         gridContainer.appendChild(gridItem);
     }
 }
-
-
-// function adjustGridSize() {
-//     let itemWidth = getWidth(gridContainer)/gridSizeInput;
-//     let itemHeight = getHeight(gridContainer)/gridSizeInput;
-
-//     let newGridContainer = document.createElement('div');
-//     newGridContainer.classList.add('grid');
-//     newGridContainer.id = 'grid-container'
-
-//     for (let i = 0; i < currentResolution ** 2; i++) {
-//         let gridItem = document.createElement('div');
-//         gridItem.className = 'grid-item';
-//         gridItem.style.width = `${itemWidth}px;`;
-//         gridItem.style.height = `${itemHeight}px`;
-//         gridItem.addEventListener('mouseover', gridItemHover);
-
-//         newGridContainer.appendChild(gridItem);
-//     }
-
-//     // newGridContainer.style.backgroundColor = `red`;
-//     // gridContainer.style.gridTemplateColumns = `repeat(${currentResolution}, ${itemWidth}px);`;
-//     // gridContainer.style.gridTemplateRows = `repeat(${currentResolution}, ${itemHeight}px)`
-//     gridContainer.replaceWith(newGridContainer);
-//     // gridContainer.style.gridTemplateRows = `repeat(9, minmax(10px, 10vw));`;
-//     document.getElementById('grid-container').style.gridTemplateColumns = `auto auto auto auto;`;
-
-// }
 
 function adjustGridSize() {
     oldResolution = currentResolution;
@@ -156,6 +142,35 @@ function adjustGridSize() {
     
 }
 
+
+function handleAboutClick(){
+    modal.classList.remove('hide');
+
+    modalTitle.textContent = 'About';
+
+    let modalBodyText = `<p>This web page was made by pklepa as an early learning experience in web development.<br>
+        <br>
+        You can see more of my work at <a style='font-weight: bold; color: white' href="https://github.com/pklepa">https://github.com/pklepa</a></p>`;
+    modalBody.innerHTML = modalBodyText;
+}
+
+
+function handleHelpClick(){
+    modal.classList.remove('hide');
+
+    modalTitle.textContent = 'Help'
+
+    let modalBodyText = `<p>Etch-a-Sketch is a popular mechanical drawing toy invented in the late 1950s by Andre Cassagnes. 
+    The toy was a hit for the greater part of the second half of the 20th century and is still today recognized and beloved by those who come across it.<br>
+    <br>
+    This site aims to recreate the original concept of the toy by implementing a pixelized grid in which you can use your imagination and draw on!<br>
+    <br>
+    Although the main objective of this project is to exercise and learn ever bit more of web development, I'll still leave the invitation for you to have fun with it.<br>
+    <br>
+    That's it! Thanks for visiting.    
+    </p>`;
+    modalBody.innerHTML = modalBodyText;
+}
 
 
 
